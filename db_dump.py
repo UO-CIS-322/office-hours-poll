08b624580d57f00152079cb780733d901759b149
Michal Young <michal@cs.uoregon.edu>
2016-01-22 14:11:05 -0800

    Dump the database

---
+++ db_dump.py
@@ -0,0 +1,28 @@
+"""
+Dump the MongoDB database as text
+"""
+
+# Mongo database
+from pymongo import MongoClient
+import CONFIG
+try: 
+    dbclient = MongoClient(CONFIG.MONGO_URL)
+    db = dbclient.cis322
+    collection = db.poll
+
+except:
+    print("Failure opening database.  Is Mongo running? Correct password?")
+    sys.exit(1)
+
+
+#
+# Read database --- May be useful to see what is in there,
+# even after you have a working 'insert' operation in the flask app,
+# but they aren't very readable.  If you have more than a couple records,
+# you'll want a loop for printing them in a nicer format. 
+#
+
+records = [ ] 
+for record in collection.find( { "kind": "office hours poll" } ):
+    print(record)
+
