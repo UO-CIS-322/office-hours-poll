11dd6387d41f1044a04adfbe196b69fe69d913d1
Michal Young <michal.young@gmail.com>
2016-03-11 21:03:17 -0800

    As used in winter 2016

--- flask_poll.py
+++ flask_poll.py
@@ -27,17 +27,9 @@ except:
 
 
 # Half-hour periods 
-CHOICES = [ { "day": "Tuesday",
-              "periods": [ "11:30a", "12:00p", "12:30p", "1:00p", "1:30p",
-                           "2:00p", "2:30p", "3:00p", "5:00p", "5:30p" ]}, 
-            { "day": "Wednesday",
-              "periods": [ "10:00a", "10:30a", "11:00a", "11:30a", "12:00p",
-                           "2:30p", "3:00p", "3:30p", "4:00p", "4:30p", "5:00p",
-                           "5:30p" ]}, 
-            { "day": "Thursday",
-              "periods": [ "9:00a", "9:30a", "11:30a", "12:00p", "12:30p", "1:00p", "1:30p",
-                           "2:00p", "2:30p", "3:00p", "5:00p", "5:30p" ]}, 
-            ]
+from choices import CHOICES
+
+
 ###
 # Pages
 ###
